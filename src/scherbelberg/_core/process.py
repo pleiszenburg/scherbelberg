82065479f58e4acb54b1cbc648096cca8d8e2e61
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-16 16:49:03 +0100

    can wait for list of processes, with error handling

--- src/scherbelberg/_core/process.py
+++ src/scherbelberg/_core/process.py
@@ -27,12 +27,14 @@ specific language governing rights and limitations under the License.
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+from logging import Logger
 from subprocess import Popen
+from time import sleep
 from typing import List, Tuple, Union
 
 from typeguard import typechecked
 
-from .abc import ProcessABC
+from .abc import CommandABC, ProcessABC
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # CLASS
@@ -47,10 +49,16 @@ class Process(ProcessABC):
     Mutable.
     """
 
-    def __init__(self, procs = List[Popen]):
+    def __init__(self, procs: List[Popen], command: CommandABC):
 
         self._procs = procs
-        self._communicated = True
+        self._command = command
+
+        self._completed = False
+        self._output = []
+        self._errors = []
+        self._status = []
+        self._exception = None
 
 
     def __call__(self, returncode: bool = False) -> Union[
@@ -58,32 +66,15 @@ class Process(ProcessABC):
         Tuple[List[str], List[str]],
     ]:
 
-        if not self._communicated:
-            raise SystemError('process communicated its result')
-        self._communicated = False
-
-        output, errors, status = [], [], []
-
-        for proc in self._procs[::-1]:  # inverse order, last process first
-
-            out, err = proc.communicate()
-            output.append(self._com_to_str(out))
-            errors.append(self._com_to_str(err))
-            status.append(int(proc.returncode))
-
-        output.reverse()
-        errors.reverse()
-        status.reverse()
-
-        exception = SystemError("command failed", str(self), output, errors)
+        self._complete()
 
         if returncode:
-            return output, errors, status, exception
+            return self._output, self._errors, self._status, self._exception
 
-        if any((code != 0 for code in status)):  # some fragment failed:
-            raise exception
+        if any((code != 0 for code in self._status)):
+            raise self._exception
 
-        return output, errors
+        return self._output, self._errors
 
 
     @property
@@ -93,6 +84,34 @@ class Process(ProcessABC):
             proc.poll() is not None
             for proc in self._procs
         ))
+
+
+    @property
+    def command(self) -> CommandABC:
+
+        return self._command
+
+
+    def _complete(self):
+
+        if self._completed:
+            return
+
+        for proc in self._procs[::-1]:  # inverse order, last process first
+
+            out, err = proc.communicate()
+            self._output.append(self._com_to_str(out))
+            self._errors.append(self._com_to_str(err))
+            self._status.append(int(proc.returncode))
+
+        self._output.reverse()
+        self._errors.reverse()
+        self._status.reverse()
+        self._exception = SystemError(
+            "command failed", str(self._command), self._output, self._errors,
+        )
+
+        self._completed = True
 
 
     @staticmethod
@@ -108,3 +127,54 @@ class Process(ProcessABC):
                 return repr(com)
 
         return com
+
+
+    @staticmethod
+    def wait_for(
+        comment: str,
+        procs: List[ProcessABC],
+        log: Logger,
+        wait: float,
+        assert_sucess: bool = True,
+    ):
+
+        assert wait > 0.0
+
+        log.info(f'Waiting for: {comment:s} ...')
+
+        while True:
+            running = len([proc for proc in procs if proc.running])
+            if running == 0:
+                break
+            log.info(f'{running:d} out of {len(procs):d} are still running ...')
+            sleep(wait)
+
+        log.info(f'Processes exited: {comment:s}')
+
+        if not assert_sucess:
+            log.info(f'Did not assess success of processes: {comment:s}')
+            return
+
+        if all((
+            all((code == 0 for code in proc(returncode = True)[2]))
+            for proc in procs
+        )):
+            log.info(f'Processes were successful: {comment:s}')
+            return
+
+        log.error(f'At least one process failed: {comment:s}')
+
+        for proc in procs:
+            if all((code == 0 for code in proc(returncode = True)[2])):
+                continue
+            log.error(f'Command failed: {str(proc.command):s}')
+            for idx, out, err, status, in zip(range(len(proc.command)), *proc(returncode = True)[:3]):
+                if status == 0:
+                    continue
+                log.error(f'Command fragment {idx:d} failed. Output:')
+                if len(out.strip()) > 0:
+                    log.error(out.strip())
+                if len(err.strip()) > 0:
+                    log.error(err.strip())
+
+        raise SystemError('A process did not exit with code 0.')
