3affeae890bba9a9291bbfea52e5ecaa17a1d73a
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-17 12:16:30 +0100

    dask launchers

---
+++ src/scherbelberg/share/bootstrap_worker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# SCHERBELBERG
+# HPC cluster deployment and management for the Hetzner Cloud
+#
+# https://github.com/pleiszenburg/scherbelberg
+#
+#     src/scherbelberg/share/bootstrap_worker.sh: Start worker
+#
+#     Copyright (C) 2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+#
+# <LICENSE_BLOCK>
+# The contents of this file are subject to the BSD 3-Clause License
+# ("License"). You may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# https://github.com/pleiszenburg/scherbelberg/blob/master/LICENSE
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+# specific language governing rights and limitations under the License.
+# </LICENSE_BLOCK>
+
+# run as user
+
+SCHEDULER=$1
+PORT=$2
+DASHPORT=$3
+
+nohup dask-worker --dashboard-address $DASHPORT tcp://$SCHEDULER:$PORT > worker_out 2> worker_err < /dev/null &
