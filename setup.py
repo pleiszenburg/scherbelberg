045cb045242020cbca89ca388fcbca9721b17672
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-29 11:30:19 +0100

    dev deps

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ with open(os.path.join(SRC_DIR, "scherbelberg", "__init__.py"), "r", encoding="u
 base_require = ["click", "hcloud", "pyyaml", "typeguard",]
 extras_require = {
     "base": base_require,
-    "dev": ["black", "python-language-server[all]", "setuptools", "twine", "wheel",],
+    "dev": ["black", "python-language-server[all]", "setuptools", "sphinx", "sphinx_rtd_theme", "twine", "wheel",],
 }
 extras_require["all"] = list(
     {rq for target in extras_require.keys() for rq in extras_require[target]}
