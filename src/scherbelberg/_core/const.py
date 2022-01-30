3b34c74b46d3893ac3ad2325af0b2f44e9ebb2df
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-30 14:16:08 +0100

    black

--- src/scherbelberg/_core/const.py
+++ src/scherbelberg/_core/const.py
@@ -27,9 +27,9 @@ specific language governing rights and limitations under the License.
 # CONST
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-HETZNER_INSTANCE_TINY = 'cx11'
-HETZNER_IMAGE_UBUNTU = 'ubuntu-20.04'
-HETZNER_DATACENTER = 'fsn1-dc14'
+HETZNER_INSTANCE_TINY = "cx11"
+HETZNER_IMAGE_UBUNTU = "ubuntu-20.04"
+HETZNER_DATACENTER = "fsn1-dc14"
 
 WORKERS = 1
 
